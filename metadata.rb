823464b56bc922765465ae5c1a8d8fddb1bc9175
Tim Smith <tsmith84@gmail.com>
2015-09-30 18:20:53 -0700

    Add long_description and supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,8 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs the chef-vault gem and provides chef_vault_item recipe helper'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.3.1'
+
+source_url 'https://github.com/chef-cookbooks/chef-vault' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/chef-vault/issues' if respond_to?(:issues_url)
