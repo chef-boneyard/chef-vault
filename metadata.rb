6ad7e34f9b860dc1b339a0c1814214595098e4b0
Tim Smith <tsmith84@gmail.com>
2016-03-14 11:35:34 -0700

    Release 1.3.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs the chef-vault gem and provides chef_vault_item recipe helper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.3.2'
+version '1.3.3'
 
 source_url 'https://github.com/chef-cookbooks/chef-vault' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/chef-vault/issues' if respond_to?(:issues_url)
