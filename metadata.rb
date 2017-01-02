df7f723f886082ca3438d899be874504a700890e
Thom May <thom@chef.io>
2017-01-02 08:42:44 +0000

    Release 3.0
    Signed-off-by: Thom May <thom@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs the chef-vault gem and provides chef_vault_item recipe helper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.1.1'
+version '3.0.0'
 
 source_url 'https://github.com/chef-cookbooks/chef-vault'
 issues_url 'https://github.com/chef-cookbooks/chef-vault/issues'
 chef_version '>= 12.9'
 
-depends 'compat_resource', '>= 12.16.1'
+depends 'compat_resource', '>= 12.16.3'
 gem 'chef-vault'
