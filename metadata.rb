6e0acbc53c8dd7f95ad6b72c9139837aa45f42e6
Tim Smith <tsmith@chef.io>
2017-04-24 23:17:29 -0700

    Use a SPDX standard license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'chef-vault'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs the chef-vault gem and provides chef_vault_item recipe helper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.1.1'
