3458f95483a64768587a14b86e365362e6f3cb81
SM064722 <SM064722@cerner.net>
2019-12-31 13:16:45 -0600

    Add logic to use 3.x gem on chef 12
    Signed-off-by: SM064722 <SM064722@cerner.net>

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,16 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs the chef-vault gem and provides chef_vault_item recipe helper'
-version '3.1.1'
+version '3.1.2'
 
 source_url 'https://github.com/chef-cookbooks/chef-vault'
 issues_url 'https://github.com/chef-cookbooks/chef-vault/issues'
 chef_version '>= 12.9'
 
-gem 'chef-vault'
+if %x(chef-client --version).split[1].to_f < 13
+  gem 'chef-vault', '< 4.0'
+else
+  gem 'chef-vault'
+end
 
 supports 'any'
