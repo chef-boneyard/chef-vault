63f76da6ef92579809f7d093b9e150f1c40adcc2
Tim Smith <tsmith84@gmail.com>
2020-01-02 12:44:32 -0800

    Release 3.1.2
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs the chef-vault gem and provides chef_vault_item recipe helper'
-version '3.1.1'
+version '3.1.2'
 
 source_url 'https://github.com/chef-cookbooks/chef-vault'
 issues_url 'https://github.com/chef-cookbooks/chef-vault/issues'
