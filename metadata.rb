61f6a6a67dde8f5835879c88856b52122063ec7d
Stuart Harland <stuartharland@Stuarts-MacBook-Pro.local>
2020-01-02 14:41:39 +0000

    Fix Chef-vault gem version so it is compatible with LL infrastructure

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,12 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs the chef-vault gem and provides chef_vault_item recipe helper'
-version '3.1.1'
+version '3.1.2'
 
 source_url 'https://github.com/chef-cookbooks/chef-vault'
 issues_url 'https://github.com/chef-cookbooks/chef-vault/issues'
 chef_version '>= 12.9'
 
-gem 'chef-vault'
+gem 'chef-vault', '< 4'
 
 supports 'any'
