0755096906dcd212079048388e9fb83222155c5d
Lamont Granquist <lamont@scriptkiddie.org>
2020-04-01 12:33:25 -0700

    bump version to 4.0.1
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs the chef-vault gem and provides chef_vault_item recipe helper'
-version '4.0.0'
+version '4.0.1'
 
 source_url 'https://github.com/chef-cookbooks/chef-vault'
 issues_url 'https://github.com/chef-cookbooks/chef-vault/issues'
